5e8354f8e608177cc3154da25258e30df1c1bd58
f.lino1934 <f.lino1934@hotmail.com>
2020-05-09 19:20:39 -0300

    Configuração inicial

---
+++ src/main/java/com/unimonte/amar/ProjetoamarApplication.java
@@ -0,0 +1,13 @@
+package com.unimonte.amar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoamarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoamarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimonte/amar/ProjetoamarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimonte.amar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoamarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
